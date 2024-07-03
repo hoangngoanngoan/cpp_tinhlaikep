942c3d4baa43c680963c903ba124f36063a2850b
SPC <SPC@18-LAB402>
2024-07-03 19:33:15 +0700

    Add project files.

---
+++ cpp_tinhlaikep/cpp_tinhlaikep.cpp
@@ -0,0 +1,33 @@
+// cpp_tinhlaikep.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    double tiengui; cout << "Nhap so tien gui: "; cin >> tiengui;
+    double tienlai; 
+    double tienloi;
+    double laisuat; laisuat = 0.08;
+    //cout << "Nhap lai suat: "; cin >> laisuat;
+    int sonam; cout << "Nhap so nam gui: "; cin >> sonam;
+    
+    for (int i = 1; i <= sonam; i++) {
+        tienlai = tiengui * laisuat;
+        cout << "Tien lai nam thu: " << i  << " la: " << tienlai << endl;
+        tiengui = tienlai + tiengui;
+        cout << "Tong tien nam thu: " << i << " la: " << tiengui << endl;
+    }    
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
